9f2751cbdf4dea228e5b488cb5dbba4d70f32e88
xqqxzzxyy@gmail.com <xqqxzzxyy@gmail.com>
2021-11-01 14:16:08 +0900

    能run了属于是

--- src/LatLongUTMConvert.py
+++ src/LatLongUTMConvert.py
@@ -1,30 +1,49 @@
-import geodesy
+#!/usr/bin/env python
+import sys
+from actionlib.action_client import GoalManager
+from rospy.client import init_node
 from sensor_msgs.msg import NavSatFix
+from tf.transformations import quaternion_from_euler
+import numpy as np
 import rospy
-from geometry_msgs.msg import Pose, Quaternion
-from geodesy import utm, wu_point
-import time
 import actionlib
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseResult, MoveBaseFeedback
+import tf
+import actionlib
+import tf2_ros
+from geometry_msgs.msg import Pose, Quaternion
+from geodesy import utm
+from math import *
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-class gps_point:
-    lat = 0.0
-    lon = 0.0
-    theta= 0.0
-toxy=wu_point.WuPoint.toPointXY
 
-def feedback_callback(feedback):
-    rospy.loginfo(str(feedback))
+def next_pos(lat,lon):
 
-goal=MoveBaseGoal()
-goal.target_pose.header.frame_id = "map"
-goal.target_pose.header.stamp    = rospy.get_rostime()
-# goal.target_pose.pose = 
+    rospy.loginfo("Moving to (%f,%f)" %(lat,lon))
 
-LLtoUTM = utm.fromLatLong
-tess=LLtoUTM(36.00341,139.39113)
-rospy.loginfo(str(tess.northing))
-rospy.loginfo(str(tess.easting))
+    point = utm.fromLatLong(lat,lon)
+    # 取得当前位置
+    # pos = tf2_ros.Buffer.lookup_transform(self,"map","base_link",rospy.Time(0),rospy.Duration(2.0))
+    pos_curr = utm.fromLatLong(36.00341,139.39296)
+    # 计算现在位置与目标点的角度
+    angle_to_goal = atan2(point.northing - pos_curr.northing , point.easting - pos_curr.easting)
+    odom_quat = quaternion_from_euler(0, 0,angle_to_goal)
+
+    goal=MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp    = rospy.get_rostime()
+    goal.target_pose.pose.position.x = point.easting
+    goal.target_pose.pose.position.y = point.northing
+    goal.target_pose.pose.orientation.x = odom_quat[0]
+    goal.target_pose.pose.orientation.y = odom_quat[1]
+    goal.target_pose.pose.orientation.z = odom_quat[2]
+    goal.target_pose.pose.orientation.w = odom_quat[3]
+    return goal
+    
+
+def on_shutdown(self):
+    rospy.loginfo("Canceling all goals")
+    client.cancel_all_goals()
 
 if __name__ == '__main__':
     #节点初始化
@@ -36,9 +55,11 @@ if __name__ == '__main__':
     rospy.loginfo('Waiting for action Server '+action_server_name)
     client.wait_for_server()
     rospy.loginfo('Action Server Found...'+action_server_name)
-    #发送goal
-    client.send_goal(goal, feedback_cb=feedback_callback)
-
+    lat=36.00341
+    lon=139.39113
+    client.send_goal(next_pos(lat,lon))
+    client.wait_for_result()
+    
 
 
 
